89d0d285ea60556ec297f225ef395e60dc0b5e97
Thiago <thiagoj_@hotmail.com>
2020-09-22 19:06:41 -0300

    Foram adicionadas novas funções

--- solucoes/rainha/main.c
+++ solucoes/rainha/main.c
@@ -8,8 +8,24 @@ char tabuleiro[50][50];
 int posColuna[50];
 int numSolucoes=0;
 
-void novaFuncao() {
-	printf("teste 123");
+void novaFuncao2() {
+	printf("teste 2");
+}
+
+void novaFuncao3() {
+	printf("teste 3");
+}
+
+void novaFuncao4() {
+	printf("teste 4");
+}
+
+void novaFuncao5() {
+	printf("teste 5");
+}
+
+void novaFuncao6() {
+	printf("teste 6");
 }
 
 int verificarposColunaicao(int lin, int col, int n) {
